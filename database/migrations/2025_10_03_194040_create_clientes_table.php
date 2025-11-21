3eaee3dca1cc7a356c5ab9536b9db4372556e254
Nicolas Gonzalez <ng33446@gmail.com>
2025-11-21 12:44:55 -0500

    feat: creacion modulo clientes

---
+++ database/migrations/2025_10_03_194040_create_clientes_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+  /**
+   * Run the migrations.
+   */
+  public function up(): void
+  {
+    Schema::create('clientes', function (Blueprint $table) {
+      $table->id();
+      $table->integer('id_documento_identificacion');
+      $table->string('identificacacion');
+      $table->integer('dv')->nullable();
+      $table->string('empresa')->nullable();
+      $table->string('nombre_comercial')->nullable();
+      $table->string('nombres')->nullable();
+      $table->string('direccion')->nullable();
+      $table->string('email')->nullable();
+      $table->string('n_celular')->nullable();
+      $table->integer('tipo_organizacion');
+      $table->integer('id_tributo');
+      $table->foreignId('id_municipio')->constrained('municipios');
+      $table->timestamps();
+    });
+  }
+
+  /**
+   * Reverse the migrations.
+   */
+  public function down(): void
+  {
+    Schema::dropIfExists('clientes');
+  }
+};
